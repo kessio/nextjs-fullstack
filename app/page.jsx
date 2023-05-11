bf4e5fd32f955a527cbe1af5069da54446172e80
kessio <sharonkessio6@gmail.com>
2023-05-11 13:46:17 +0300

    homepage created

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+     <p className="desc text-center">Promptopia is an open source prompt AI Prompting tool for modern world to
+       discover, create and share modern day prompts 
+       </p>
+       {/* Feed */}
+    </section>
+  )
+}
+
+export default Home
